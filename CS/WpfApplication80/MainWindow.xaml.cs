b668a59ec9b6881fc07cf61f649d884d6dd85d7f
AndreySlabov <andrey.slabov@devexpress.com>
2022-06-20 14:43:28 +0400

    Update sample

--- CS/WpfApplication80/MainWindow.xaml.cs
+++ CS/WpfApplication80/MainWindow.xaml.cs
@@ -1,27 +1,11 @@
 ﻿using DevExpress.Export;
 using DevExpress.Export.Xl;
 using DevExpress.XtraPrinting;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApplication80 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
@@ -34,30 +18,21 @@ namespace WpfApplication80 {
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
-            XlsxExportOptionsEx options = new XlsxExportOptionsEx() { UnboundExpressionExportMode = DevExpress.Export.UnboundExpressionExportMode.AsFormula, LayoutMode = LayoutMode.Table };
+            XlsxExportOptionsEx options = new XlsxExportOptionsEx() { 
+                UnboundExpressionExportMode = UnboundExpressionExportMode.AsFormula, 
+                LayoutMode = LayoutMode.Table 
+            };
             options.BeforeExportTable += options_BeforeExportTable;
             view.ExportToXlsx("Document.xlsx", options);
             Process.Start("Document.xlsx");
         }
-
         void options_BeforeExportTable(BeforeExportTableEventArgs e) {
-            e.Table.Style.Name = XlBuiltInTableStyleId.Dark4;
+            e.Table.Style.Name = XlBuiltInTableStyleId.Light4;
         }
     }
-
     public class Item {
-        public string Name {
-            get;
-            set;
-        }
-
-        public int MinValue {
-            get;
-            set;
-        }
-        public int MaxValue {
-            get;
-            set;
-        }
+        public string Name { get; set; }
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
     }
 }
